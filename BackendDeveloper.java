613e8461d4c05997c75bb33078d6235798af954b
muhammadhanan7175 <98551709+muhammadhanan7175@users.noreply.github.com>
2022-09-12 20:47:39 +0500

    Create BackendDeveloper.java

---
+++ BackendDeveloper.java
@@ -0,0 +1,10 @@
+public class BackendDeveloper extends Developer{
+String rank;
+public BackendDeveloper(){
+
+}
+public void Salary(){
+System.out.println("Salary of Backend Developer is 65k");	
+} 
+
+}
