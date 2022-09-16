fe1b081fba2999dc92ef8c3d4cbaac41d3c6c8b8
muhammadhanan7175 <98551709+muhammadhanan7175@users.noreply.github.com>
2022-09-16 20:41:14 +0500

    Create Bhains.java

---
+++ Bhains.java
@@ -0,0 +1,12 @@
+public class Bhains implements Maan{
+public void tiddi(){
+	System.out.println("Maan has big tiddi");
+}
+public void curve(){
+	System.out.println("Maans tiddi is curved");
+}
+public void bhains(){
+	System.out.println("Maan  is a bhains");
+}
+
+}
